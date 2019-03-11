2b6bed75751aec02dd48048c4084b7aa3780af19
Graham Dominick <graham.dom17@gmail.com>
2019-03-11 17:50:31 -0600

    Add functions to set pieces and 'move path' checking

--- Player.cpp
+++ Player.cpp
@@ -3,33 +3,133 @@ using namespace std;
 
 Player::Move::Move(Square &start, Square &end): start_square(start), end_square(end){}
 
-Player::Player(bool in_color): color(in_color), other_player(nullptr){}
+Player::Player(bool in_color, Board &board)
+: color(in_color), other_player(nullptr), board(board){
+	setup_pieces();
+}
+
+void Player::setup_pieces()
+{
+	set_king();
+	set_queen();
+	set_rooks();
+	set_bishops();
+	set_knights();
+	set_pawns();
+}
+
+
+void Player::set_pawns()
+{
+	const int row = (color)? 1:6;
+	for (int col = 0; col < 8; ++col){
+		pieces.emplace_back(new Pawn(color));
+		pieces.back()->place(board[col][row]);
+	}
+}
+
+void Player::set_knights()
+{
+	const int row = (color)? 0:7;
+	pieces.emplace_back(new Knight(color));
+	pieces.back()->place(board[1][row]);
+	pieces.emplace_back(new Knight(color));
+	pieces.back()->place(board[6][row]);
+}
+
+
+void Player::set_bishops()
+{
+	const int row = (color)? 0:7;
+	pieces.emplace_back(new Bishop(color));
+	pieces.back()->place(board[2][row]);
+	pieces.emplace_back(new Bishop(color));
+	pieces.back()->place(board[5][row]);
+}
+
+void Player::set_rooks()
+{
+	const int row = (color)? 0:7;
+	pieces.emplace_back(new Rook(color));
+	pieces.back()->place(board[0][row]);
+	pieces.emplace_back(new Rook(color));
+	pieces.back()->place(board[7][row]);
+}
+
+void Player::set_queen()
+{
+	const int row = (color)? 0:7;
+	pieces.emplace_back(new Queen(color));
+	pieces.back()->place(board[3][row]);
+}
+
+void Player::set_king()
+{
+	const int row = (color)? 0:7;
+	pieces.emplace_back(new King(color));
+	pieces.back()->place(board[4][row]);
+}
 
 const Square& Player::king_location() const
 {
-	return *pieces[0].get_square();
+	return *pieces[0]->get_square();
 }
 
-/*
-Move get_input_move() const
+bool Player::is_valid_destination_square(const Move &move) const
 {
-	TODO
+	return move.end_square.is_empty() || move.end_square.get_piece()->color != color;
 }
-*/
 
-void Player::give_set_of_pieces(vector<ChessPiece> in_pieces)
+bool Player::path_is_clear(const Move &move) const
 {
-	pieces.swap(in_pieces);
+	const vector<const Square*> move_path = squares_on_move_path(move);
+
+	for (auto sqr : move_path)
+	{
+		if (!sqr->is_empty()){
+			return false;
+		}
+	}
+
+	return true;
 }
 
-/*
-bool is_valid_move(const Move &move) const
+vector<const Square*> Player::squares_on_move_path(const Move &move) const
 {
+	const int horizontal_squares_traveled = abs(move.end_square.X - move.start_square.X);
+	const int vertical_squares_traveled = abs(move.end_square.Y - move.start_square.Y);
 
+	const int h_direction = (move.end_square.X < move.start_square.X)? -1:1;
+	const int v_direction = (move.end_square.Y < move.start_square.Y)? -1:1;
+
+	vector<const Square*> move_path;
+
+	if (horizontal_squares_traveled == vertical_squares_traveled){
+		for (int i = 1; i < horizontal_squares_traveled; ++i){
+			move_path.push_back(
+				&board[move.start_square.X + (i*h_direction)][move.start_square.Y + i*v_direction]);
+		}
+	}
+
+	else if (horizontal_squares_traveled == 0 && vertical_squares_traveled > 1){
+		for (int i = 1; i < vertical_squares_traveled; ++i){
+			move_path.push_back(&board[move.start_square.X][move.start_square.Y + i*v_direction]);
+		}
+	}
+
+	else if (horizontal_squares_traveled > 1 && vertical_squares_traveled == 0){
+		for (int i = 1; i < horizontal_squares_traveled; ++i){
+			move_path.push_back(&board[move.start_square.X + i*h_direction][move.start_square.Y]);
+		}
+	}
+
+	return move_path;
 }
 
-bool is_valid_destination_square(const Move &move)
-{
 
-}
-*/
+
+
+
+
+
+
--- Player.h
+++ Player.h
@@ -1,16 +1,19 @@
 #include "ChessPiece.h"
 #include "Square.h"
+#include "Board.h"
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include <memory>
 
 class Player
 {
 	// Data
 	const bool color;
 	bool in_check = false;
-	std::vector<ChessPiece> pieces;
+	std::vector<std::unique_ptr<ChessPiece>> pieces;
 	Player *other_player;
-	//static Board board;
+	Board &board;
 
 	struct Move
 	{
@@ -21,14 +24,21 @@ class Player
 
 	// Methods
 
+	void setup_pieces();
+	void set_pawns();
+	void set_knights();
+	void set_bishops();
+	void set_rooks();
+	void set_queen();
+	void set_king();
+
 	const Square& king_location() const;
 	Move get_input_move() const;
 
-	void give_set_of_pieces(std::vector<ChessPiece> in_pieces);
-
 	bool is_valid_move(const Move &move) const;
-	bool is_valid_destination(const Move &move) const;
+	bool is_valid_destination_square(const Move &move) const;
 	bool path_is_clear(const Move &move) const;
+	std::vector<const Square*> squares_on_move_path(const Move &move) const;
 	bool move_puts_player_in_check(const Move * move);
 	
 	void move_piece(const Move &move);
@@ -37,6 +47,6 @@ class Player
 	void call_check();
 	bool in_check_mate();
 public:
-	Player(bool in_color);
-	void take_turn();
+	Player(bool in_color, Board &board);
+	//void take_turn();
 };
