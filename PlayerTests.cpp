3a95e6ac00bb8fe952af369185414c31bf243da4
Graham Dominick <graham.dom17@gmail.com>
2019-03-26 21:41:44 -0600

    Add testing for take turn function, 4 turns max

--- PlayerTests.cpp
+++ PlayerTests.cpp
@@ -6,7 +6,16 @@ int main(){
 	Board board;
 	Player p1(true, board);
 	Player p2(false, board);
+	p1.introduce_other_player(&p2);
+	p2.introduce_other_player(&p1);
 	board.display();
-
+	p1.take_turn();
+	board.display();
+	p2.take_turn();
+	board.display();
+	p1.take_turn();
+	board.display();
+	p2.take_turn();
+	board.display();
 	return 0;
 }
